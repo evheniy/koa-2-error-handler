72e6349ca5c16e769ab586e60f0be093d4480963
Evheniy Bystrov <evheniy.bystrov@just-eat.com>
2017-02-04 17:05:07 +0000

    new server tests

--- tests/server.js
+++ tests/server.js
@@ -1,20 +1,32 @@
+/*eslint no-console: 0*/
 const EventEmitter = require('events');
+const sinon = require('sinon');
 const chai = require('chai');
 const expect = chai.expect;
 const serverHandler = require('./../index').server;
-let server;
+let server, stub;
 
 describe('Server test', () => {
 
-    beforeEach(done => {
+    beforeEach(() => {
         server = new EventEmitter();
-        done();
+        stub = Object.getOwnPropertyDescriptor(process, 'exit');
+        Object.defineProperty(process, 'exit', {
+            value: code => expect(code).to.be.equal(1)
+        });
+        sinon.stub(console, 'error', text => expect(text).to.be.equal(text));
+    });
+
+    afterEach(() => {
+        server = new EventEmitter();
+        Object.defineProperty(process, 'exit', stub);
+        console.error.restore();
     });
 
     it('it should test listening event with pipe', done => {
         server.address = () => 'test';
-        serverHandler(server, bind => {
-            expect(bind).to.be.equal('pipe test');
+        serverHandler(server, address => {
+            expect(address).to.be.equal('pipe test');
             done();
         });
         server.emit('listening');
@@ -22,10 +34,73 @@ describe('Server test', () => {
 
     it('it should test listening event with port', done => {
         server.address = () => ({ port: 3000});
-        serverHandler(server, bind => {
-            expect(bind).to.be.equal('port 3000');
+        serverHandler(server, address => {
+            expect(address).to.be.equal('port 3000');
             done();
         });
         server.emit('listening');
     });
+
+    it('it should test clientError event with port', done => {
+        const error = new Error('test');
+        const socet = {
+            end(text) {
+                expect(text).to.be.equal('HTTP/1.1 400 Bad Request\r\n\r\n');
+            }
+        };
+        server.address = () => ({ port: 3000});
+        serverHandler(server, null, err => {
+            expect(err).to.be.equal(error);
+            done();
+        });
+        server.emit('clientError', error, socet);
+    });
+
+    it('it should test error event without syscall', done => {
+        const error = new Error('test');
+        error.code = 'test';
+        server.address = () => ({ port: 3000});
+        serverHandler(server, null, err => {
+            expect(err).to.be.equal(error);
+            done();
+        });
+        server.emit('error', error);
+    });
+
+    it('it should test error event', done => {
+        const error = new Error('test');
+        error.syscall = 'listen';
+        error.code = 'test';
+        server.address = () => ({ port: 3000});
+        serverHandler(server, null, err => {
+            expect(err).to.be.equal(error);
+            done();
+        });
+        server.emit('error', error);
+    });
+
+    it('it should test error event with code: EACCES', done => {
+        const error = new Error('test');
+        error.syscall = 'listen';
+        error.code = 'EACCES';
+        server.address = () => ({ port: 3000});
+        serverHandler(server, null, err => {
+            expect(err).to.be.equal(error);
+            done();
+        });
+        server.emit('error', error);
+    });
+
+    it('it should test error event with code: EADDRINUSE', done => {
+        const error = new Error('test');
+        error.syscall = 'listen';
+        error.code = 'EADDRINUSE';
+        server.address = () => ({ port: 3000});
+        serverHandler(server, null, err => {
+            expect(err).to.be.equal(error);
+            done();
+        });
+        server.emit('error', error);
+    });
+    
 });
